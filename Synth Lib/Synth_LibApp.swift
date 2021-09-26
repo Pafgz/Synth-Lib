b1fc4ee805d2199b0a7ca193372543c67fbb0854
pierre-antoinefagniez <pierre-antoine.fagniez@realifetech.com>
2021-09-26 17:32:37 +0200

    Initial Commit

---
+++ Synth Lib/Synth_LibApp.swift
@@ -0,0 +1,17 @@
+//
+//  Synth_LibApp.swift
+//  Synth Lib
+//
+//  Created by Pierre-Antoine Fagniez on 26/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Synth_LibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
